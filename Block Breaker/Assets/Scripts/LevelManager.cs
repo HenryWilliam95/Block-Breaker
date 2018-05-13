9a022752f41cd9fcac72405ea32f72d81f0fccfe
Henry Crofts <hwcrofts@gmail.com>
2018-05-13 15:33:55 +0100

    Initial commit

---
+++ Block Breaker/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+    // Reference to the button the script sits on
+    public Button button;
+
+    public void LoadLevel(int level)
+    {
+        // Load the level specified in inspector
+        SceneManager.LoadScene(level);
+    }
+
+    public void Quit()
+    {
+        // Quit the application
+        Application.Quit();
+        print("Application quit");
+    }
+}
---
+++ Block Breaker/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Static variable for singleton
+    public static MusicPlayer instance = null;
+
+    private void Awake()
+    {
+        // Moved code to Awake to stop any duplicate players
+
+        if (instance != null)
+        {
+            print("Destroying Awake Music Player");
+            Destroy(gameObject);
+        }
+        else
+        { 
+            // Cause background music to continue playing across scenes
+            GameObject.DontDestroyOnLoad(gameObject);
+
+            // Set the instance to the first music player
+            instance = this;
+        }
+    }
+}

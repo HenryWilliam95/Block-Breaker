2ca32c555158d9fc883c8ccf8e1fb35ecb8c279d
Henry Crofts <hwcrofts@gmail.com>
2018-05-14 00:12:50 +0100

    Updates

---
+++ Block Breaker/Assets/Scripts/paddle.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paddle : MonoBehaviour {
+
+    Vector3 paddlePos;
+    public float speed = 5;
+
+    // Use this for initialization
+    void Start ()
+    {
+        paddlePos = new Vector3(this.transform.position.x, this.transform.position.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(this.transform.position.x + h * speed * Time.deltaTime, this.transform.position.y);
+
+        this.transform.position = movement;
+
+        if(this.transform.position.x > 7.3)
+        {
+            this.transform.position = new Vector3(7.3f, this.transform.position.y);
+        }
+        else if (this.transform.position.x < -7.3)
+        {
+            this.transform.position = new Vector3(-7.3f, this.transform.position.y);
+        }
+    }
+}
---
+++ Block Breaker/Assets/ball.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball : MonoBehaviour {
+    public paddle Paddle;
+
+    private Vector3 paddleToBallVector;
+
+    Rigidbody2D rb;
+    bool started = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        paddleToBallVector = this.transform.position - Paddle.transform.position;
+
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(!started)
+            this.transform.position = Paddle.transform.position + paddleToBallVector;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            started = true;
+            rb.velocity = new Vector2(2.0f, 10.0f);
+        }
+
+        
+	}
+}
